1f24e90a50c4d8678e7aecb392d983da9dfa9388
dxdxlili <dxdxlili.@163.com>
2019-05-24 16:49:39 +0800

    branch second

--- neural_network1.py
+++ neural_network1.py
@@ -1,1 +1,30 @@
-
+import torch
+import numpy
+#python的加减乘除
+x=10
+y=20
+z=x+y
+print(z)
+z1=x*y
+print(z)
+z2=x-y
+print(z2)
+z3=x/y
+print(z3)
+#python的字符串和数字
+str="你好！"
+print(str)
+#python的元组
+str=(1,2,3,4)
+print(str[1])
+#python的列表
+lists=[1,2,3,4,5]
+print(lists)
+lists.append(6)
+print(lists)
+dict={"name":"lili","age":21}
+print(dict)
+dict['one']=1
+dict['two']=2
+print(dict)
+print(dict.keys())
